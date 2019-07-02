87fba7ebb8c5e39c3529da5be7a4cd6ed853f4cd
Socks the Fox <jade@socksthefox.net>
2019-07-02 15:32:45 -0500

    Add PHPUnit testing and an initial set of tests

---
+++ tests/IntervalTest.php
@@ -0,0 +1,31 @@
+<?php
+
+define("START_2019",	1546300800);
+define("STAMP_2019",	1562098538);
+define("END_2019",		1577836800);
+
+use PHPUnit\Framework\TestCase;
+
+final class IntervalTest extends TestCase
+{
+	public function testTimestamp()
+	{
+		$interval = new \CF\Chrono\Interval();
+		$interval->onTimestamp(STAMP_2019);
+		
+		$this->assertContains(STAMP_2019, $interval->getIntervalsBetween(START_2019, END_2019));
+	}
+	
+	public function testMinutes()
+	{
+		$interval = new \CF\Chrono\Interval();
+		
+		$interval->every(15, "minutes");
+		
+		$values = [];
+		for($i = START_2019; $i < END_2019; $i += 15*60)
+			$values []= $i;
+		
+		$this->assertEquals($values, $interval->getIntervalsBetween(START_2019, END_2019));
+	}
+}
