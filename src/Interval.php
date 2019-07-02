328f0dc70c58695be64091cdaca8c11059bd21db
Socks the Fox <jade@socksthefox.net>
2019-07-02 15:09:21 -0500

    Adds `getIntervalsBetween` which is basically the whole reason this exists

--- src/Interval.php
+++ src/Interval.php
@@ -10,14 +10,15 @@ class Interval
 	private $month = null;
 	private $day = null;
 	
-	private $hour = null;
-	private $minute = null;
+	private $hour = 0;
+	private $minute = 0;
 	
 	private $endOfMonth = false;
 	private $dayOfWeek = null;
 	private $interval = null;
 	
 	private $type = null;
+	private $tz = null;
 	
 	/*
 		Accepts a string representing the interval, or null
@@ -59,22 +60,18 @@ class Interval
 			until June 8th, 2019 at 1:20 PM UTC
 	*/
 	
-	public function __construct($interval = null)
+	public function __construct($interval = null, $timezone = "UTC")
 	{
+		$this->timezone($timezone);
+		// If the user doesn't specify an interval
+		// let's default to daily.
+		if($interval === null)
+			$interval = "daily";
 		$this->set($interval);
 	}
 	
 	public function set($intervalStr)
 	{
-		// If the user doesn't specify an interval
-		// let's default to daily.
-		if($intervalStr === null)
-		{
-			$this->type = "day";
-			$this->daysInterval = 1;
-			return;
-		}
-		
 		$char1 = $intervalStr[0];
 		
 		$intervalParts = explode(';', $intervalStr);
@@ -118,6 +115,11 @@ class Interval
 		}
 	}
 	
+	public function timezone($timezone)
+	{
+		$this->tz = new \DateTimeZone($timezone);
+	}
+	
 	public function onTimestamp($timestamp)
 	{
 		$this->type = "timestamp";
@@ -149,7 +151,7 @@ class Interval
 		}
 		else if(in_array($stride, $days))
 		{
-			$this->type = "date";
+			$this->type = "weekday";
 			$this->dayOfWeek = $stride;
 		}
 		return $this;
@@ -164,7 +166,7 @@ class Interval
 		if($date == "eom")
 			$this->endOfMonth = true;
 		else if(in_array($date, $days))
-			$this->dayOfWeek = $date;
+			$this->every(1,$date);
 		else if(strpos($date, '/') !== false)
 		{
 			$monthDay = explode('/', $date);
@@ -181,7 +183,7 @@ class Interval
 	{
 		// Minute/hour based intervals can't be run at a specific time
 		if($this->type == "minute")
-			return $this;
+			throw new \Exception("Minute/hour based intervals can't be run at a specific time");
 		
 		// Parse the time
 		$a = explode(' ', $time);
@@ -219,5 +221,118 @@ class Interval
 	{
 		$this->end = $timestamp;
 		return $this;
+	}
+	
+	public function getIntervalsBetween($starting, $ending)
+	{
+		// Timestamps are easy! Just check if it's between the start and end times
+		//  and if so, return it.
+		if($this->type == "timestamp")
+		{
+			if($this->timestamp >= $starting && $this->timestamp <= $ending)
+				return [$this->timestamp];
+			else
+				return [];
+		}
+		
+		// No intervals if we're starting past the set end
+		if($this->end !== null && $starting >= $this->end)
+			return [];
+		
+		// Also no intervals if we're ending before the set start
+		if($ending < $this->start)
+			return [];
+		
+		$realStart = max($this->start, $starting);
+		$realEnd = $ending;
+		if($this->ending !== null)
+			$realEnd = min($this->end, $ending);
+		
+		$result = [];
+		
+		if($this->type == "minute")
+		{
+			// We're on a minutes-based interval
+			// We need to find the integer X in
+			//  $this->start + X * $minutes >= $realStart
+			
+			$minutes = $this->interval * 60000;
+			$startDiff = $realStart - $this->start;
+			$X = intval(ceil($startDiff/$minutes));
+			
+			// This is the first proper interval after the start
+			$i = $this->start + $X * $minutes;
+			
+			if($i > $realEnd)
+				return [];
+			
+			// Get all the intervals until the end
+			$result = range($i, $realEnd, $minutes);
+		}
+		else if($this->type == "day")
+		{
+			// Almost the same as with minutes but with some
+			//  added annoyingness thanks to being run at a specific
+			//  time of day
+			$startDT = new DateTime("@{$this->start}");
+			$startDT->setTimezone($this->tz);
+			$startDT->setTime($this->hour,$this->minute,0);
+			$realStartDT = new DateTime("@{$realStart}");
+			$realStartDT->setTimezone($this->tz);
+			
+			$endDT = new DateTime("@{$realEnd}");
+			
+			$startDiff = $startDT->diff($realStartDT);
+			
+			$X = intval(ceil($startDiff->days / $this->interval));
+			$XDT = new DateInterval("{$X}D");
+			$i = new DateTime("@{$this->start}");
+			$i->setTimezone($this->tz);
+			$i->setTime($this->hour,$this->minute,0);
+			$i->add($XDT);
+			
+			$interval = new DateInterval("{$this->interval}D");
+			
+			if($i > $endDT)
+				return [];
+			
+			do
+			{
+				$result[] = $i->getTimestamp();
+				$i->add($interval);
+			}
+			while($i <= $endDT);
+		}
+		else
+		{
+			// Weekdays and dates are basically the same
+			$realStartDT = new DateTime("@{$realStart}");
+			$realStartDT->setTimezone($this->tz);
+			
+			
+			
+			$endDT = new DateTime("@{$realEnd}");
+		}
+		
+		return $result;
+	}
+	
+	private getDateTimeInfo($dateTime)
+	{
+		$str = $dateTime->format("Y n j G i D t");
+		$vals = explode(" ", $str);
+		
+		$result = [
+			'year' => $vals[0],
+			'month' => $vals[1],
+			'day' => $vals[2],
+			'hour' => $vals[3],
+			'minute' => $vals[4],
+			'weekday' => strtolower($vals[5]),
+			'eom' => $vals[2] == $vals[6],
+			'ord' => ($vals[2] % 7) + 1
+		];
+		
+		return $result;
 	}
 }
